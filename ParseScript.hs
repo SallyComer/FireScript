5a88c69264352c09cd4053f46fb913f1329ef126
unknown <rodent05@gmail.com>
2015-03-19 09:08:06 -0500

    Switched parser to use Either. added operator parsing, verified list parsing

--- ParseScript.hs
+++ ParseScript.hs
@@ -5,17 +5,26 @@ import ScanScript
 
 data Statement = Assign String Expr
     | Do Expr
-    | Keyword Expr deriving (Show, Eq)
+    | Block [Statement]
+    | While Expr Statement
+    | If Expr Statement
+    | Else Statement
+    | Elif Statement
+    | For Statement Expr Statement
+    | Declare String
+    | DeclAssign String Expr
+    | Return Expr deriving (Show, Eq)
 
 
 
 data Expr = Number Int
     | Str String
     | Parens Expr
-    | Operator Expr String Expr
+    | Operator String Expr Expr
     | Call String [Expr]
     | List [Expr]
     | Name String deriving (Show, Eq)
+
 
 
 
@@ -26,26 +35,31 @@ data Declaration = FuncDec String [String] [Statement]
 data Program = Program [Declaration]
 
 
-type Parser a = [Token] -> Maybe (a, [Token])
+type Parser a = [Token] -> Either String (a, [Token])
 
 
 parseCommaStuff :: Parser [Expr]
 parseCommaStuff a = case parseExpr a of
-    Just (thing, (Comma:rest)) -> case parseCommaStuff rest of
-        Just (things, rest') -> Just (thing:things, rest')
-    Nothing -> Just ([], a)
+    Right (thing, (Comma:rest)) -> case parseCommaStuff rest of
+        Right (things, rest') -> Right (thing:things, rest')
+    Right (thing, rest) -> Right ([thing], rest)
+    Left _ -> Right ([], a)
 
 parseExpr :: Parser Expr
-parseExpr (a:(OperatorT op):rest) =
-    let
-        Just (arg1, _) = parseExpr [a]
-        Just (arg2, rest2) = parseExpr rest
-    in Just (Operator arg1 op arg2, rest2)
-parseExpr ((NameT name):LParen:rest) = case parseCommaStuff rest of
-    Just (args, (RParen:rest')) -> Just (Call name args, rest')
-parseExpr (NameT a:rest) = Just (Name a, rest)
-parseExpr (StrT a:rest) = Just (Str a, rest)
-parseExpr (LParen:rest) = case parseExpr rest of
-    Just (thing, (RParen:rest')) -> Just (Parens thing, rest')
-parseExpr (LBracket:rest) = case parseCommaStuff rest of
-    Just (items, (RBracket:rest')) -> Just (List items, rest')
+parseExpr ((NameT name):LParen:rest) = parseOp $ case parseCommaStuff rest of
+    Right (args, (RParen:rest')) -> Right (Call name args, rest')
+parseExpr (NumberT a:rest) = parseOp $ Right (Number a, rest)
+parseExpr (NameT a:rest) = parseOp $ Right (Name a, rest)
+parseExpr (StrT a:rest) = parseOp $ Right (Str a, rest)
+parseExpr (LParen:rest) = parseOp $ case parseExpr rest of
+    Right (thing, (RParen:rest')) -> Right (Parens thing, rest')
+parseExpr (LBracket:rest) = parseOp $ case parseCommaStuff rest of
+    Right (items, (RBracket:rest')) -> Right (List items, rest')
+    a -> Left (show a)
+parseExpr a = Left (show a)
+
+parseOp (Right (arg1, (OperatorT op:rest))) = case parseExpr rest of
+    Right (arg2, rest') -> Right (Operator op arg1 arg2, rest')
+parseOp a = a
+
+parseText = parseExpr . tokenize
